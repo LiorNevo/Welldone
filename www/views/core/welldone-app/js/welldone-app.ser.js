fb96256be80dc25f9c0301dc52fb535220762682
root <root@lior-Work>
2016-05-17 19:05:27 +0300

    Almost completing the views

---
+++ www/views/core/welldone-app/js/welldone-app.ser.js
@@ -0,0 +1,122 @@
+/*----------------------------------------------------------------------------------------------------*/
+/**
+ * Category Services
+ */
+angular.module('welldoneApp').factory('loadAllCategories',
+		 	[ function() {
+		 		return function() {
+		 			try {
+						var categories = angular.fromJson(localStorage
+								.getItem("welldoneCategories"));
+					} catch (e) {
+						categories = [{}];
+					}
+					if (!categories) {
+		 				categories = [{}];
+		 			}
+		 			return categories;
+		 		}
+		 	} ]);
+
+angular.module('welldoneApp').factory('saveCategory',
+		 	[ 'loadAllCategories', function(loadAllCategories) {
+		 		return function(saveCategory) {
+		 			var categories = loadAllCategories();
+		 			var found = false;
+		 			var index = 0;
+		 			var lastId = 0;
+		 			angular.forEach(categories, function(category){
+		 				if (category.id >= lastId){
+		 					lastId = category.id + 1;
+		 				}
+		 				if (category.id == saveCategory.id){
+		 					found = true;
+		 					categories.splice(index, 1);
+		 					if (!saveCategory.id){
+		 						saveCategory.id = lastId;
+		 					}
+		 					categories.push(saveCategory);
+		 				}
+		 				index++;
+		 			});
+		 			if (!found){
+		 				saveCategory.id = lastId;
+		 				categories.push(saveCategory);
+		 			}
+		 			localStorage.setItem("welldoneCategories", angular.toJson(categories));
+		 		}
+		 	} ]);
+angular.module('welldoneApp').factory('deleteCategory',
+	 	[ 'loadAllCategories', function(loadAllCategories) {
+	 		return function(deletCategory) {
+	 			var oldCategories = loadAllCategories();
+	 			var newCategories = [];
+	 			angular.forEach(oldCategories, function(category){
+	 				if (category.id != deletCategory.id){
+	 					newCategories.push(category);
+	 				}
+	 			});
+	 			localStorage.setItem("welldoneCategories", angular.toJson(newCategories));
+	 		}
+	 	} ]);
+/*----------------------------------------------------------------------------------------------------*/
+/**
+ * Location Services
+ */
+angular.module('welldoneApp').factory('loadAllLocations',
+		 	[ function() {
+		 		return function() {
+		 			try {
+						var locations = angular.fromJson(localStorage
+								.getItem("welldoneLocations"));
+					} catch (e) {
+						locations = [{}];
+					}
+					if (!locations) {
+		 				locations = [{}];
+		 			}
+		 			return locations;
+		 		}
+		 	} ]);
+
+angular.module('welldoneApp').factory('saveLocation',
+		 	[ 'loadAllLocations', function(loadAllLocations) {
+		 		return function(saveLocation) {
+		 			var locations = loadAllLocations();
+		 			var found = false;
+		 			var index = 0;
+		 			var lastId = 0;
+		 			angular.forEach(locations, function(location){
+		 				if (location.id >= lastId){
+		 					lastId = location.id + 1;
+		 				}
+		 				if (location.id == saveLocation.id){
+		 					found = true;
+		 					locations.splice(index, 1);
+		 					if (!saveLocation.id){
+		 						saveLocation.id = lastId;
+		 					}
+		 					locations.push(saveLocation);
+		 				}
+		 				index++;
+		 			});
+		 			if (!found){
+		 				saveLocation.id = lastId;
+		 				locations.push(saveLocation);
+		 			}
+		 			localStorage.setItem("welldoneLocations", angular.toJson(locations));
+		 		}
+		 	} ]);
+angular.module('welldoneApp').factory('deleteLocation',
+	 	[ 'loadAllLocations', function(loadAllLocations) {
+	 		return function(deletLocation) {
+	 			var oldLocations = loadAllLocations();
+	 			var newLocations = [];
+	 			angular.forEach(oldLocations, function(location){
+	 				if (location.id != deletLocation.id){
+	 					newLocations.push(location);
+	 				}
+	 			});
+	 			localStorage.setItem("welldoneLocations", angular.toJson(newLocations));
+	 		}
+	 	} ]);
--- www/views/locations/js/locations.ctrl.js
+++ www/views/locations/js/locations.ctrl.js
@@ -1,3 +1,83 @@
-angular.module('locApp', []).controller('locCtrl',['$scope','closeNavBar', function($scope, closeNavBar){
-	closeNavBar();
+angular.module('locApp', []).controller('locCtrl',[
+                 '$scope',
+                 'closeSideBar',
+                 'navBarTitle',
+                 'toolbarActions',
+                 'loadAllLocations',
+                 'saveLocation',
+                 'deleteLocation',
+                 '$mdDialog',
+                 function(
+                		 $scope, 
+                		 closeSideBar, 
+                		 navBarTitle,
+                		 toolbarActions,
+                		 loadAllLocations,
+                		 saveLocation,
+                		 deleteLocation,
+                		 $mdDialog){
+	closeSideBar();
+	navBarTitle('Locations');
+	toolbarActions([{
+		name: 'Add New Location',
+		action: function(){
+			$scope.locations.push({});
+		},
+		icon: 'add'
+	}]);
+	$scope.globalAttr = globalAttr;
+	$scope.locations = loadAllLocations();
+	/*----------------------------------------------------------------------------------------------------*/
+	$scope.deleteLocation = function(location, ev, index) {
+		var confirmDelete = $mdDialog
+				.confirm()
+				.title('Delete Location?')
+				.textContent(
+						'Are you sure you want to delete ' + location.name + '?')
+				.ariaLabel('Delete Location')
+				.targetEvent(ev).ok('Yes, Delete')
+				.cancel('No');
+		$mdDialog
+				.show(confirmDelete)
+				.then(
+						function() {
+							deleteLocation(location);
+							$scope.locations.splice(index, 1);
+						},
+						function() {
+						});
+	}
+	/*----------------------------------------------------------------------------------------------------*/
+	$scope.saveLocation = function(location, ev, inValid) {
+		if (!inValid){
+			var confirmSave = $mdDialog
+			.confirm()
+			.title('Save Location?')
+			.textContent(
+					'Are you sure you want to save ' + location.name + '?')
+			.ariaLabel('Save Location')
+			.targetEvent(ev).ok('Yes, Save')
+			.cancel('No');
+			$mdDialog
+			.show(confirmSave)
+			.then(
+					function() {
+						saveLocation(location);
+					},
+					function() {
+					});
+		}
+		else {
+			var notValid = $mdDialog
+			.alert()
+			.clickOutsideToClose(true)
+			.title('Not All Fields Are Completed')
+			.textContent(
+					'You must complete all fields in order to save')
+			.ariaLabel('Alert')
+			.targetEvent(ev).ok('Ok, Got it!')
+			$mdDialog
+			.show(notValid);
+		}
+	}
 }])
