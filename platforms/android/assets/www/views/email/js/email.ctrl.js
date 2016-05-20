e249ed4e018a6032350d7a9478aaf394eef8aab4
root <root@lior-Work>
2016-05-21 00:06:52 +0300

    Fixing the file save function

--- platforms/android/assets/www/views/email/js/email.ctrl.js
+++ platforms/android/assets/www/views/email/js/email.ctrl.js
@@ -17,6 +17,18 @@ angular.module('emailApp', []).controller('emailCtrl',['$scope',
 	$scope.locations = loadAllLocations();
 	/*----------------------------------------------------------------------------------------------------*/
 	$scope.email = function(){
+		window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
+	        console.log("got main dir",dir);
+	        dir.getFile("temp.txt", {create:true}, function(file) {
+	            console.log("got the file", file);
+	            file.createWriter(function(fileWriter) {
+	    	        fileWriter.seek(fileWriter.length);
+	    	        var str = angular.toJson($scope.categories);
+	    	        fileWriter.write(str);
+	    	        console.log("ok, in theory i worked");
+	    	    }, function(){});
+	        });
+	    });
 		
 		cordova.plugins.email.isAvailable(
 			    function (isAvailable) {
