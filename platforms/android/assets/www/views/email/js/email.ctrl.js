784d3b146f3dd8e8d41963b4754c0f423b80a6b8
root <root@lior-Home>
2016-05-21 01:41:00 +0300

    Finished setting up the email transfer via file save

--- platforms/android/assets/www/views/email/js/email.ctrl.js
+++ platforms/android/assets/www/views/email/js/email.ctrl.js
@@ -4,32 +4,56 @@ angular.module('emailApp', []).controller('emailCtrl',['$scope',
                                                        'toolbarActions',
                                                        'loadAllLocations',
                                                        'loadAllCategories',
+                                                       '$filter',
                                          function($scope, 
                                         		 closeSideBar, 
                                         		 navBarTitle, 
                                         		 toolbarActions,
                                         		 loadAllLocations,
-                                        		 loadAllCategories){
+                                        		 loadAllCategories,
+                                        		 $filter){
 	closeSideBar();
 	navBarTitle('Email');
 	toolbarActions([]);
 	$scope.categories = loadAllCategories();
 	$scope.locations = loadAllLocations();
+	$scope.locationsFileCreated = false;
+	$scope.categoriesFileCreated = false;
+	$scope.toEmails = [];
 	/*----------------------------------------------------------------------------------------------------*/
-	$scope.email = function(){
-		window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(dir) {
-	        console.log("got main dir",dir);
-	        dir.getFile("temp.txt", {create:true}, function(file) {
-	            console.log("got the file", file);
+	$scope.newChip = function($chip){
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if (re.test($chip)){
+			$scope.toEmails.push($chip);
+		}
+		return null;
+	}
+	/*----------------------------------------------------------------------------------------------------*/
+	$scope.createFiles = function(){
+		$scope.timestamp = $filter('date')(new Date(), 'yyyyMMddHHmmss');
+		window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir) {
+	        dir.getFile("categories" + $scope.timestamp + ".txt", {create:true}, function(file) {
 	            file.createWriter(function(fileWriter) {
 	    	        fileWriter.seek(fileWriter.length);
 	    	        var str = angular.toJson($scope.categories);
 	    	        fileWriter.write(str);
-	    	        console.log("ok, in theory i worked");
+	    	        $scope.categoriesFileCreated = true;
 	    	    }, function(){});
 	        });
+	        dir.getFile("locations" + $scope.timestamp + ".txt", {create:true}, function(file) {
+	            file.createWriter(function(fileWriter) {
+	    	        fileWriter.seek(fileWriter.length);
+	    	        var str = angular.toJson($scope.locations);
+	    	        fileWriter.write(str);
+	    	        $scope.locationsFileCreated = true;
+	    	    }, function(){});
+	        });
+	        
 	    });
-		
+	};
+	$scope.createFiles();
+	/*----------------------------------------------------------------------------------------------------*/
+	$scope.email = function(){
 		cordova.plugins.email.isAvailable(
 			    function (isAvailable) {
 			    	if (isAvailable){
@@ -40,14 +64,16 @@ angular.module('emailApp', []).controller('emailCtrl',['$scope',
 			    	}
 			    }
 			);
-		
 	}
 	/*----------------------------------------------------------------------------------------------------*/
 	$scope.sendTheEmail = function(){
+		if (!$scope.categoriesFileCreated || !$scope.locationsFileCreated) return;
 		cordova.plugins.email.open({
-		    to:      'max@mustermann.de',
-		    subject: 'Greetings',
-		    body:    'How are you? Nice greetings from Leipzig'
+		    to:      $scope.toEmails,
+		    subject: 'myLocations Categories and Locations',
+		    attachments: [cordova.file.externalDataDirectory + "categories" + $scope.timestamp + ".txt", 
+		                  cordova.file.externalDataDirectory + "locations" + $scope.timestamp + ".txt"],
+		    body:    'Hi, attached are the files containing information regarding your Categories and Locations'
 		});
 	}
 }])
